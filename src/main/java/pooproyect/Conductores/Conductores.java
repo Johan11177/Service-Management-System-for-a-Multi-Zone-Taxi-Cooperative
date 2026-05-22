9abe18d14ae73385425118c176348ecd6d4a388e
Michael Velasquez <edwilz777@gmail.com>
2026-05-22 02:14:29 -0500

    Avance servicio

--- src/main/java/pooproyect/Conductores/Conductores.java
+++ src/main/java/pooproyect/Conductores/Conductores.java
@@ -1,9 +1,78 @@
 package pooproyect.Conductores;
 
-public class Conductores {
-    private String nombre;
-    private String ID;
-    private String zonaDeTrabajo;
-    private Boolean disponibilidad;
+import java.util.ArrayList;
 
+import pooproyect.Servicio.Servicio;
+
+public class Conductores {
+
+    private String nombre;
+    private boolean disponible;
+
+    private ArrayList<Servicio> servicios;
+
+    public Conductores(String nombre) {
+
+        this.nombre = nombre;
+        this.disponible = true;
+
+        servicios = new ArrayList<>();
+    }
+
+    public void agregarServicio(Servicio servicio) {
+
+        servicios.add(servicio);
+    }
+
+    public boolean puedeAtender(String tipoServicio) {
+
+        for (Servicio servicio : servicios) {
+
+            if (servicio.getNombre()
+                    .equalsIgnoreCase(tipoServicio)) {
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void ocuparConductor() {
+
+        disponible = false;
+    }
+
+    public void liberarConductor() {
+
+        disponible = true;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public ArrayList<Servicio> getServicios() {
+        return servicios;
+    }
+    
+
+    @Override
+    public String toString() {
+
+        String texto = "";
+
+        for (Servicio servicio : servicios) {
+
+            texto += servicio.getNombre() + " ";
+        }
+
+        return "Conductor: " + nombre +
+               "\nDisponible: " + disponible +
+               "\nServicios: " + texto;
+    }
 }
--- src/main/java/pooproyect/Servicio/Servicio.java
+++ src/main/java/pooproyect/Servicio/Servicio.java
@@ -1,8 +1,29 @@
 package pooproyect.Servicio;
 
-public class Servicio {
-    
-public void finalizarServicio() {
-System.out.println("Servicio finalizado");
-}
+import pooproyect.Tarifas.Tarifa;
+
+public abstract class Servicio {
+
+    protected String nombre;
+    protected Tarifa tarifa;
+
+    public Servicio(String nombre, Tarifa tarifa) {
+
+        this.nombre = nombre;
+        this.tarifa = tarifa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Tarifa getTarifa() {
+        return tarifa;
+    }
+
+    @Override
+    public String toString() {
+
+        return nombre;
+    }
 }
